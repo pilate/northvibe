5a2bfcc92215d850f8474223148acd9144dea55c
Pilate <noptical@gmail.com>
2023-08-05 15:18:29 -0700

    add log function

--- mi2c.cpp
+++ mi2c.cpp
@@ -1,6 +1,9 @@
 #include <Wire.h>
 #include "mi2c.h"
 
+
+#define SERIAL_I2C 0x11
+
 uint8_t I2C_Write(uint8_t addr, uint8_t reg, uint8_t value) {
   Wire.beginTransmission(addr);
   Wire.write(reg);
@@ -18,4 +21,11 @@ uint8_t I2C_StartRead(uint8_t addr, uint8_t reg, int count) {
 uint8_t I2C_ReadByte(uint8_t addr, uint8_t reg) {
   I2C_StartRead(addr, reg, 1);
   return (uint8_t)Wire.read();
+}
+
+void I2C_LogString(char* message, uint8_t size) {
+  Wire.beginTransmission(SERIAL_I2C);
+  Wire.write(0x0);
+  Wire.write(message, size);
+  Wire.endTransmission();
 }
--- mi2c.h
+++ mi2c.h
@@ -1,8 +1,12 @@
 #ifndef MI2C_h
 #define MI2C_h
 
+#include <Arduino.h>
+
 uint8_t I2C_Write(uint8_t addr, uint8_t reg, uint8_t value);
 uint8_t I2C_StartRead(uint8_t addr, uint8_t reg, int count);
 uint8_t I2C_ReadByte(uint8_t addr, uint8_t reg);
+
+void I2C_LogString(char *message, uint8_t size);
 
 #endif
