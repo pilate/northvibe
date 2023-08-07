bfa68072ff4c6881a812038d61c1325a87162fa8
Pilate <noptical@gmail.com>
2023-08-07 09:08:15 -0700

    add kriswiner's libs

--- MMC5983MA.h
+++ MMC5983MA.h
@@ -1,10 +1,54 @@
+/*
+ * Copyright (c) 2020 Tlera Corp.  All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal with the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ *  1. Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimers.
+ *  2. Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimers in the
+ *     documentation and/or other materials provided with the distribution.
+ *  3. Neither the name of Tlera Corp, nor the names of its contributors
+ *     may be used to endorse or promote products derived from this Software
+ *     without specific prior written permission.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+ * CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * WITH THE SOFTWARE.
+ * 
+ * The MMC5983MA is an AEC-Q100 qualified complete 3-axis magnetic sensor with on-chip 
+ * signal processing and integrated I2C/SPI bus suitable for use in automotive applications.
+ * Couple with the LSM6DSM accel/gyro and LPS22HB barometer the system provides a compact and
+ * accurate absolute orientation estimation engine.
+ * 
+ * Uses the I2C interface for data transfer.
+ * 
+ * Intended to be run on a Dragonfly development board:
+ * 
+ * https://www.tindie.com/products/tleracorp/dragonfly-stm32l47696-development-board/
+ *  
+ * 
+ * Library may be used freely and without limit with attribution.
+ */
+
 #ifndef MMC5983MA_h
 #define MMC5983MA_h
 
 #include "Arduino.h"
+#include <Wire.h>
+#include "I2Cdev.h"
 
-#define MMC5983MA_ADDRESS       0x30
-
+//Register map for MMC5983MA'
+//http://www.memsic.com/userfiles/files/DataSheets/Magnetic-Sensors-Datasheets/MMC5983MA_Datasheet.pdf
 #define MMC5983MA_XOUT_0        0x00
 #define MMC5983MA_XOUT_1        0x01
 #define MMC5983MA_YOUT_0        0x02
@@ -20,7 +64,7 @@
 #define MMC5983MA_CONTROL_3     0x0C
 #define MMC5983MA_PRODUCT_ID    0x2F // Should be 0x30
 
-#define MMC5983MA_CONTROL_0_TM_M 0x01
+#define MMC5983MA_ADDRESS       0x30
 
 // Sample rates
 #define MODR_ONESHOT   0x00
@@ -49,31 +93,29 @@
 #define MSET_1000  0x06
 #define MSET_2000  0x07
 
+
 class MMC5983MA
 {
-  private:
-    uint8_t rawData[7];
   public:
-    // MMC5983MA(I2Cdev* i2c_bus);
-    uint8_t getProductID();
-    void read();
-    // void init(uint8_t MODR, uint8_t MBW, uint8_t MSET);
-    // void offsetBias(float * dest1, float * dest2);
-    // void reset();
-    uint8_t status();
-    // void clearInt();
-    // void selfTest();
-    // void readData(uint32_t * destination);
-    // uint8_t readTemperature();
-    // void SET();
-    // void RESET();
-    // void getOffset(float * destination);
-    // void powerDown();
-    // void powerUp(uint8_t MODR);
+  MMC5983MA(I2Cdev* i2c_bus);
+  uint8_t getChipID();
+  void init(uint8_t MODR, uint8_t MBW, uint8_t MSET);
+  void offsetBias(float * dest1, float * dest2);
+  void reset();
+  uint8_t status();
+  void clearInt();
+  void selfTest();
+  void readData(uint32_t * destination);
+  uint8_t readTemperature();
+  void SET();
+  void RESET();
+  void getOffset(float * destination);
+  void powerDown();
+  void powerUp(uint8_t MODR);
   
-  // private:
-  // float _mRes;
-  // I2Cdev* _i2c_bus;
+  private:
+  float _mRes;
+  I2Cdev* _i2c_bus;
 };
 
 #endif
